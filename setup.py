085322e5e3fd6fbdea158d4daeb10a2370fd7189
Ah777ed_said <ahmed01226313482@gmail.com>
2025-01-12 19:52:27 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup
 
 setup(
-    name="my_python_project",  # اسم المشروع
-    version="0.1",  # رقم الإصدار
-    py_modules=["my_script"],  # اسم الملف .py بدون الامتداد
-    install_requires=[],  # إذا كان لديك مكتبات أخرى يجب تثبيتها مع المشروع
+    name="eyEar",  
+    version="0.1",  
+    py_modules=["my_script"],  
+    install_requires=[], 
     entry_points={
         "console_scripts": [
-            "my-python-project=my_script:hello_world",  # أمر لتشغيل الكود من الطرفية
+            "my-python-project=my_script:hello_world",
         ],
     },
 )
